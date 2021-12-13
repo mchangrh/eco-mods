675b4bc0d583a42fa9c162086fb1a5377e88f312
Michael C <michael@mchang.name>
2021-12-12 22:20:21 -0500

    add stockPileRange

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ add subfolder in `Mods/UserCode/` or something, I haven't played the game yet
 
 alwaysLuckyHands: Always have lucky hands (mining)
 intLimitVehicles: Vehicles with the carry capacity of C#'s integer limit
+stockPileRange: Increase lumber stockpile range
---
+++ stockPileRange/Objects/LumberStockpileObject.override.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Strange Loop Games. All rights reserved.
+// See LICENSE file in the project root for full license information.
+
+namespace Eco.Mods.TechTree
+{
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.Objects;
+    using Eco.Shared.Math;
+
+    [RequireComponent(typeof(PublicStorageComponent))]
+    [RequireComponent(typeof(StockpileComponent))]
+    [RequireComponent(typeof(WorldStockpileComponent))]
+    public partial class LumberStockpileObject : WorldObject
+    {
+        public static readonly Vector3i DefaultDim = new Vector3i(8, 8, 8);
+
+        protected override void OnCreate()
+        {
+            base.OnCreate();
+            StockpileComponent.ClearPlacementArea(this.Creator, this.Position3i, DefaultDim, this.Rotation, 1);
+        }
+
+        protected override void PostInitialize()
+        {
+            base.PostInitialize();
+            
+            this.GetComponent<StockpileComponent>().Initialize(DefaultDim, 1);
+            
+            var storage = this.GetComponent<PublicStorageComponent>();
+            storage.Initialize(DefaultDim.x * DefaultDim.z);
+            storage.Storage.AddInvRestriction(new StockpileStackRestriction(DefaultDim.y * 10)); // limit stack sizes to the y-height of the LumberStockpile
+
+            this.GetComponent<LinkComponent>().Initialize(30);
+        }
+    }
+}
